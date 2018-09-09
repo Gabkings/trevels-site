ed02851d24fca1c41f14a1f0133c49c7c1e63977
unknown <gabworks51@gmail.com>
2018-09-09 04:00:42 +0300

    added browser Sync funtionality

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var gulp = require('gulp'),
     postcss = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
     cssvars = require('postcss-simple-vars'),
-    nested = require('postcss-nested');
+    nested = require('postcss-nested'),
+    cssImports = require('postcss-import'),
+    browserSync = require('browser-sync').create();
 
 gulp.task('default', function(){
     console.log("You create a gulp task");
@@ -13,16 +15,28 @@ gulp.task('html', function(){
     console.log("html task being run here");
 });
 gulp.task('styles', function(){
-    return gulp.src('./assets/styles/styles.css')
-                .pipe(postcss([nested, cssvars, autoprefixer])) 
-                .pipe(gulp.dest('./assets/temp/styles'))
+    return gulp.src('./app/assets/styles/styles.css')
+                .pipe(postcss([cssImports, nested, cssvars, autoprefixer])) 
+                .pipe(gulp.dest('./app/assets/temp/styles'))
 
 });
 gulp.task('watch',function(){
-    watch('./index.html',function(){
-        gulp.start('html');
+    browserSync.init({
+        notify: false,
+        server:{
+            baseDir:"app"
+        }
+    })
+
+    watch('./app/index.html',function(){
+        browserSync.reload();
     });
-    watch('./assets/styles/**/*.css',function(){
-        gulp.start('styles');
+    watch('./app/assets/styles/**/*.css',function(){
+        gulp.start('cssInject');
     });
+});
+
+gulp.task('cssInject', ['styles'] , function(){
+    return gulp.src('./app/assets/temp/styles/style.css')
+    .pipe(browserSync.stream());
 });
